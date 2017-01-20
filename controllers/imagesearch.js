b43b6aa30ee3dcd36e7c9e69df7401b00298d388
szib <szib@users.noreply.github.com>
2017-01-20 10:27:26 +0000

    add history

--- controllers/imagesearch.js
+++ controllers/imagesearch.js
@@ -34,7 +34,7 @@ class GoogleClient {
         if (start) {
             options.start = parseInt(start);
         }
-        
+
         return qs.stringify(options);
     }
 
@@ -51,11 +51,46 @@ class GoogleClient {
 
 var googleClient = new GoogleClient(process.env.GOOGLE_CX, process.env.GOOGLE_API_KEY);
 
+function saveHistory(query) {
+    var mongodb = require('mongodb');
+    var MongoClient = mongodb.MongoClient;
+    var mongoUrl = process.env.IMAGESEARCH_HISTORY_URI;
+
+    var moment = require('moment');
+    var timestamp = moment(new Date()).unix();
+    var document = {
+        'time': timestamp,
+        'what': query
+    };
+    console.log(document);
+    MongoClient.connect(mongoUrl, (err, db) => {
+        if (err) {
+            console.log('Database error: ', err);
+            document.err = err;
+        } else {
+            console.log('Connection established.');
+            var collection = db.collection('history');
+
+            collection.insert(document, (err, result) => {
+                if (err) {
+                    console.log("Insert error: ", err);
+                    document = {};
+                    document.err = err;
+                } else {
+                    console.log('Document has been inserted.')
+                }
+                db.close();
+            });
+        }
+    });
+}
+
 router.get('/imagesearch/:QUERY', (request, response) => {
     googleClient.search(request.params.QUERY).then((res) => {
         response.set('Content-type', 'application/json');
         response.status(200);
         response.send(res).end();
+        saveHistory(request.params.QUERY);
     });
 });
 
@@ -64,7 +99,33 @@ router.get('/imagesearch/:QUERY/offset/:OFFSET', (request, response) => {
         response.set('Content-type', 'application/json');
         response.status(200);
         response.send(res).end();
+        saveHistory(request.params.QUERY);
     });
+});
+
+router.get('/imagesearch_history', (request, response) => {
+    var mongodb = require('mongodb');
+    var MongoClient = mongodb.MongoClient;
+    var mongoUrl = process.env.IMAGESEARCH_HISTORY_URI;
+
+    MongoClient.connect(mongoUrl, (err, db) => {
+        if (err) {
+            console.log('Database error: ', err);
+            document.err = err;
+        } else {
+            console.log('Connection established.');
+            var collection = db.collection('history');
+            collection.find().sort({
+                time: -1
+            }).limit(10).toArray((err, documents) => {
+                response.set('Content-type', 'application/json');
+                response.status(200);
+                response.send(documents).end();
+            });
+            db.close();
+        }
+    });
+
 });
 
 module.exports = router;
