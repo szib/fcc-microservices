d569057e46c27fd95f65d0c3f23bb0a7417c64a3
szib <szib@users.noreply.github.com>
2017-01-20 11:57:45 +0000

    add marked npm to serve md files

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+
 var express = require('express');
 var app = express();
+var path = require('path');
+var fs = require('fs');
+var markdown = require('marked');
+
+markdown.setOptions({
+	gfm: false
+});
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -7,6 +15,17 @@ app.use(require('./controllers/timestamp'));
 app.use(require('./controllers/header_parser'));
 app.use(require('./controllers/tinyurl'));
 app.use(require('./controllers/imagesearch'));
+
+app.get('/', (req, res) => {
+	var mdFile = path.join(__dirname, 'public', 'index.md');
+	var index = fs.readFile(mdFile, 'utf8', (err, data) => {
+		if (err) {
+			return console.log(err);
+		} else {
+			res.send(markdown(data.toString()));
+		}
+	})
+});
 
 // Listen
 app.listen(app.get('port'), function() {
